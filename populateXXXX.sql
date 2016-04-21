15c4a2c784dec247d5c6889811a8d34b9a946c70
Ben Morrison <Ben@Benjamins-MacBook-Pro.local>
2016-04-21 17:49:35 -0400

    list of 50 courts; completed populate.sql

--- populateXXXX.sql
+++ populateXXXX.sql
@@ -1,6 +1,6 @@
 INSERT INTO courts (courtname, courtloc, courtlat, courtlong) VALUES ('Conte Forum', 42.33514, -71.16792);
 INSERT INTO courts (courtname, courtloc, courtlat, courtlong) VALUES ('Rogers Park', 42.347881, -71.158812);
-INSERT INTO courts (courtname, courtloc, courtlat, courtlong) VALUES ('', , );
+INSERT INTO courts (courtname, courtloc, courtlat, courtlong) VALUES ('Coolidge Park', 42.346223, -71.131297);
 
 INSERT INTO games (gamename, gameloc, creator, datescheduled, players, skilllevel)
 	VALUES ('pickupgame1', 'Conte Forum', 'joeschmoe', 21-04-2016, 10, 'Beginner');
