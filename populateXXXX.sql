e13cbc119368ed5765e8c9962202de4fc0260778
Ben Morrison <Ben@Benjamins-MacBook-Pro.local>
2016-04-24 14:43:04 -0400

    more fixes on data doc

--- populateXXXX.sql
+++ populateXXXX.sql
@@ -7,7 +7,7 @@ INSERT INTO games (gamename, gameloc, creator, datescheduled, players, skillleve
 INSERT INTO games (gamename, gameloc, creator, datescheduled, players, skilllevel)
 	VALUES ('pickupgame2', 'Rogers Park', 'jordangrose', 22-04-2016, 1, 'Intermediate');
 INSERT INTO games (gamename, gameloc, creator, datescheduled, players, skilllevel)
-	VALUES ('Friday Pickup Game', '', 'benmorrison', 29-04-2016, 1, 'Advanced');
+	VALUES ('Friday Pickup Game', 'Coolidge Park', 'benmorrison', 29-04-2016, 1, 'Advanced');
 	
 INSERT INTO user_info (username, password, email, skilllevel) VALUES ('joeschmoe', 'joe123', 'joe@example.com', 'Beginner');
 INSERT INTO user_info (username, password, email, skilllevel) VALUES ('jordangrose', 'jpg123', 'grose@bc.edu', 'Intermediate');
